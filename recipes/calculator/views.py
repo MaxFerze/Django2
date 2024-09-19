b22d90704216ef445a00b5095fb711672b5d5d2b
MaxFerze <144607289+MaxFerze@users.noreply.github.com>
2024-09-19 21:22:59 +0300

    CompliteHomework

---
+++ recipes/calculator/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+def cook_book(request, dish):
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+    calculated_ingredients = {}
+    if dish in DATA:
+        for ingredient, amount in DATA[dish].items():
+            calculated_ingredients[ingredient] = amount * int(request.GET.get('servings', 1))
+    context = {
+    'recipe': calculated_ingredients,
+    'dish' : dish
+        }
+    
+    return render(request, 'calculator/index.html', context)
